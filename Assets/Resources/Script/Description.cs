2a73961c366eb14193a5e429eda20070c7118a22
whdcks522 <whdcks522@naver.com>
2023-08-13 13:40:54 +0900

    목록 추가

--- Assets/Resources/Script/Description.cs
+++ Assets/Resources/Script/Description.cs
@@ -1,2 +1,5 @@
 //PolyBrush, Basic RPG Cursors, 한국어 입력 효과, Nodachi AnimSet, EndlessBook, Tropical Forest Pack, Magic Slashes FX, Horror Bundle - Sound Effects, 2D Pixel - RPG Monster Pack
 //UI Particle Image, PUN 2 - FREE, Fantasy Skybox FREE
+
+//aa, aaaa, aab, abb, bossA
+//b6c5, aac, bcc, b bossA, a7b3c1c9. bossB 
